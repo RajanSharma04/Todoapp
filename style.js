672730410432d45acc880e621df257cc836e70ba
Rajan Sharma <sharmarajan4565@gmail.com>
2022-07-03 15:43:14 +0530

    addition

--- style.js
+++ style.js
@@ -1,13 +1,25 @@
-const button = document.querySelector('#btn2');
-const TextBox = document.querySelector('#textbox');
-const addli = document.querySelector("#addli");
+function todo() {
+    let TextBox = document.querySelector('.textbox');
+    let Button = document.querySelector('.add-btn');
+    let addli = document.querySelector('.ul-list');
+    let Start=document.createElement("Button");
+    let Text= document.createElement("Li");
+    if(TextBox.value===''){
+        alert("please enter task");
+    }
+    else
+    {
+        
+        Text.append(document.createTextNode(TextBox.value));
+        addli.append(Text);
+    }
 
-button.addEventListener('click',function(){
-    const text = document.createElement("Li");
-    const content = document.createTextNode(TextBox.value);
-    TextBox.value= ' ';
-
-    text.append(content);
-    addli.append(text);
-    alert('item add');
-});
+        let clear = document.createElement("Button");
+        clear.append(document.createTextNode("Delete"));
+        clear.style.marginLeft = '10px';
+        Text.append(clear);
+        clear.addEventListener('click', function () {
+        Text.remove();
+    });
+    
+}
