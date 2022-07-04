71017e9fa4f341c6318a44ae6e195f31c21ca732
Rajan Sharma <sharmarajan4565@gmail.com>
2022-07-04 18:49:48 +0530

    file modify

--- style.js
+++ style.js
@@ -1,45 +1,64 @@
 function Task() {
-    const TextBox = document.querySelector('.textbox');
-    const addli = document.querySelector('.ul-list'); 
-    const Text= document.createElement("Li");
+    let TextBox = document.querySelector('.textbox');
+    let addli = document.querySelector('.ul-list');
+    let Text = document.createElement("Li");
+    let btn =document.querySelector('.add-btn');
+    let resetbtn = document.createElement("button");
+    let click = document.querySelector('#clicks')
+    let i = 1;
+    let temp=0;
+    btn.addEventListener('click',function(){
+      
+        i++;
+    });
+    click.textContent= i;
+        
     
-    if(TextBox.value==''){
+    if (TextBox.value == '') {
         alert("Sorry Enter Task");
     }
-    else
-    {
+    else {
         Text.append(document.createTextNode(TextBox.value));
         addli.append(Text);
+       
         TextBox.value = '';
-    }
-        const clear = document.createElement("Button");
+
+        let clear = document.createElement("Button");
         clear.append(document.createTextNode("Delete"));
         Text.append(clear);
-        clear.addEventListener('click', function(){
-        Text.remove();
-    });
-
-
-
-    const start = document.createElement("Button");
-    const startbtn = document.createTextNode("Start");
-    start.append(startbtn);
-    Text.append(start);    
-    start.addEventListener('click', function(){
-        const progress = document.querySelector(".progress-ul");
-        progress.append(Text); 
-        start.style.display = 'none';
-        Text.append(Donebtn);
-    });
-
-    
-    const Donebtn = document.createElement("button");
-    Donebtn.append(document.createTextNode("Done"));
-
-    Donebtn.addEventListener('click',function(){
-        let complete = document.querySelector('.complete-ul');
-        complete.append(Text);
-        Donebtn.style.display = 'none';
-        alert('Task done')
+        clear.addEventListener('click', function () {
+            Text.remove();
         });
-  };
+        const start = document.createElement("Button");
+        let startbtn = document.createTextNode("Start");
+        start.append(startbtn);
+        Text.append(start);
+        start.addEventListener('click', function () {
+            let progress = document.querySelector(".progress-ul");
+            progress.append(Text);
+            start.style.display = 'none';
+            Text.append(Donebtn);
+        });
+
+
+        let Donebtn = document.createElement("button");
+        Donebtn.append(document.createTextNode("Done"));
+
+        Donebtn.addEventListener('click', function () {
+            let complete = document.querySelector('.complete-ul');
+            complete.append(Text);
+            Donebtn.style.display = 'none';
+            alert('Task done')
+        });
+
+    }
+    
+};
+
+
+// let resetbtn = document.createElement("button");
+// resetbtn.append(document.createTextNode('Reset'));
+// Text.append(resetbtn);
+// resetbtn.addEventListener('click', function () {
+//     addli.append(Text);
+// });
