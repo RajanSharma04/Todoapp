b2c4fba05bb963f8786a682b4c26ee29c1d2e0b2
Rajan Sharma <sharmarajan4565@gmail.com>
2022-07-11 09:11:24 +0530

    changes

--- todo.js
+++ todo.js
@@ -19,23 +19,41 @@ form.addEventListener('submit', function (event) {
     }
     else {
         add(text);
-        i+=1;
-        click.innerHTML = i; 
+        i += 1;
+        click.innerHTML = i;
         maintext.value = '';
     }
-    render();
+    show();
 });
 
-function render() {
+function show() {
+    var progress = document.querySelector(".progress-ul");
+    progress.innerHTML = '';
     var addli = document.querySelector('.ul-list');
     addli.innerHTML = '';
+    let complete = document.querySelector('.complete-ul');
+    complete.innerHTML = "";
+    
     for (i = 0; i < todo.length; i++) {
+        let litext = document.createElement("li");
+        litext.setAttribute('id', todo[i].id);
+        litext.append(document.createTextNode(todo[i].text));
         if (todo[i].status == 0) {
-            let litext = document.createElement("li");
-            litext.setAttribute('id', todo[i].id);
-            litext.append(document.createTextNode(todo[i].text));
             addli.append(litext);
             deletetodo(litext);
+            starttodo(litext);
+        }
+        else if (todo[i].status === 1) {
+            progress.append(litext);
+            deletetodo(litext);
+            donetodo(litext);
+           
+        }
+        else if (todo[i].status === 2){
+            complete.append(litext);
+            deletetodo(litext);
+
+
         }
     }
 }
@@ -47,16 +65,39 @@ function deletetodo(litext) {
         let deleteli = litext.getAttribute('id');
         todo = todo.filter(Todoid => Todoid.id != deleteli);
         litext.remove();
-        if(i>0){
+        if (i > 0) {
             i--;
-          click.innerHTML=i;   
-          }
+            click.innerHTML = i;
+        }
     });
 }
-let btn =document.querySelector('.add-btn');
+function starttodo(litext) {
+    let start = document.createElement("Button");
+    let startbtn = document.createTextNode("Start");
+    start.append(startbtn);
+    litext.append(start);
+    start.addEventListener('click', function () {
+        let startli = litext.getAttribute('id');
+        todo[todo.findIndex(Todoid => Todoid.id == startli)].status=1;
+        show();
+       
+    });
+}
+function donetodo(litext){
+    let Donebtn = document.createElement("button");
+    Donebtn.append(document.createTextNode("Done"));
+    litext.append(Donebtn);
+    Donebtn.addEventListener('click', function () {
+        let startli = litext.getAttribute('id');
+        todo[todo.findIndex(Todoid => Todoid.id == startli)].status=2;
+        show();
+        console.log(todo);
+
+    })
+}
+let btn = document.querySelector('.add-btn');
 let click = document.querySelector('.clicks')
-let i=0;
-btn.addEventListener('click',function(){
-      
+let i = 0;
+btn.addEventListener('click', function () {
+
 });
- 
